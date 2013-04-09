b512ae7f696b84bdb307e31d7f8e6cc3cee743c8
Zach Feldman <zach@indabamusic.com>
2013-04-09 19:14:04 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myauth::Application.config.secret_token = '9b634267d265a4794f52007bc35958a97f02cd48e5dc2d7bb0ca7ae84f496ec6ca81034a1763aa1d6dd411ef0865efc0630c2fdf6fc923e4dd50666868f508bb'
