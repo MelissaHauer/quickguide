8d03dff87542dd63fc937c1145350b5f42cca88b
Zach Feldman <zach@indabamusic.com>
2013-04-09 19:56:35 -0400

    Modified users_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new  
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to root_url, :notice => "Signed up successfully!"
+    else
+      flash[:alert] = "Sign up failed."
+      render "new"
+    end
+  end
+
 end
